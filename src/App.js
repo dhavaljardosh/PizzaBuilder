2fc49dc0e98ccb499624a5564268cf9ddfe26a11
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2020-09-23 16:53:13 -0500

    removed link tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Customize from "./components/Customize";
 import Checkout from "./components/Checkout";
 
